803ca07fb682c2ffe81b027e593e70321219c480
Demars Berhe <demarsberhe@Demarss-MBP.fios-router.home>
2022-05-20 05:00:00 -0400

    update portfolio section with links to projects, responsive layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 export function Contact(){
   return(
-      <div className="formContainer">
-      <h2 id="contact" className="sectionTitle">Contact</h2>
-      <form className="formField">
-        <label for="name"> Name</label>
-        <br/>
-        <input type="text" id="name"/>
-        <br/>
-        <label for="email">Email</label>
-        <br/>
-        <input type="text" id="email"/>
-        <br/>
-        <label for="subject">Subject</label>
-        <br/>
-        <input type="text" id="subject"/>
-        <br/>
-        <label for="message">Message</label>
-        <br/>
-        <input type="text" id="message"/>
-        <br/>
-        <input type="submit"></input>
-      </form>
+    <div className="formContainer">
+    <h2 id="contact" className="sectionTitle">Contact</h2>
+    <div className="btn-container">
+      <button className="btn"><a href="mailto:demars.berhe@gmail.com">Send Email</a></button>
     </div>
-
+    
+    </div>
   )
 }
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,20 +1,34 @@
 import React from "react";
-import {PortfolioItems} from "./PortfolioItems";
 import {PortfolioCard} from "./PortfolioCard";
-
+import recipes from "../../images/recipes.png"
+import product from "../../images/product.png";
+import weather from "../../images/weather.png";
 export function Portfolio(){
   return(
     <div className="portfolioSection">
       <h2 id="portfolio" className="sectionTitle">Portfolio</h2>
       <div className="portfolioContainer">
-        {PortfolioItems.map(item=>{
-          return <PortfolioCard
-            name={item.name}
-            summary={item.summary}
-            link={item.link}
+      <PortfolioCard
+            key={"project-1"}
+            name={"Recipes Project"}
+            summary={"A React, Firebase, Next.js application."}
+            link={"https://github.com/demarsberhe/react-recipes-project/tree/firebase-implementation"}
+            img={recipes}
           />
-        })
-      }
+          <PortfolioCard
+            key={"project-2"}
+            name={"Product Landing Page"}
+            summary={"Replicate design of Kith product landing page. A Bootstrap and SASS application."}
+            link={"https://github.com/demarsberhe/replicate-product-page/tree/main-product"}
+            img={product}
+          />
+          <PortfolioCard
+            key={"project-3"}
+            name={"Weather Application"}
+            summary={"Provides upcoming weather forecast depending on the location entered by the user. An AccuWeather API, JavaScript, Bootstrap application."}
+            link={"https://github.com/demarsberhe/recipes/tree/js-implementation"}
+            img={weather}
+          />
       </div>
     </div>
   )
